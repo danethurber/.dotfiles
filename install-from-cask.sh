8ecf0f21f95e4c57c581abcbd825e33359e06faa
Dane Thurber <dane.thurber@gmail.com>
2017-02-28 18:03:33 -0700

    update cask programs

--- install-from-cask.sh
+++ install-from-cask.sh
@@ -3,9 +3,9 @@
 brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
-brew cask install adobe-creative-cloud 2> /dev/null
-brew cask install alfred 2> /dev/null
 brew cask install atom 2> /dev/null
 brew cask install caffeine 2> /dev/null
 brew cask install iterm2 2> /dev/null
-brew cask install keyboard-maestro 2> /dev/null
+brew cask install google-chrome 2> /dev/null
+brew cask install visual-studio-code 2> /dev/null
+brew cask install 1password 2> /dev/null
