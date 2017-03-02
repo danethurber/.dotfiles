7974f995c87e0b96f4f66f7be9471867dda54928
Dane Thurber <dane.thurber@gmail.com>
2017-03-02 09:36:19 -0700

    update casks

--- install-from-cask.sh
+++ install-from-cask.sh
@@ -3,10 +3,13 @@
 brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
+brew cask install 1password 2> /dev/null
 brew cask install atom 2> /dev/null
 brew cask install caffeine 2> /dev/null
-brew cask install iterm2 2> /dev/null
 brew cask install google-chrome 2> /dev/null
-brew cask install visual-studio-code 2> /dev/null
-brew cask install 1password 2> /dev/null
+brew cask install iterm2 2> /dev/null
+brew cask install screenhero 2> /dev/null
 brew cask install slack 2> /dev/null
+brew cask install sketch 2> /dev/null
+brew cask install spotify 2> /dev/null
+brew cask install visual-studio-code 2> /dev/null
