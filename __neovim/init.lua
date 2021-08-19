59907979aa0e9f27c72fbfa68f2f297a79d3dbf2
Dane Thurber <dane.thurber@gmail.com>
2021-08-19 14:17:09 -0600

    remove old nvim config

--- __neovim/init.lua
+++
@@ -1,9 +0,0 @@
-package.path = "/Users/dane/.dotfiles/neovim/?.lua;" .. package.path
-
-require('plugins')
-require('language')
-require('config')
-require('style')
-require('dashboard')
-require('terminal')
-require('keys')
